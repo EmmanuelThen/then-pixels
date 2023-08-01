6e037604d149d67da3ca1b956e10fac0e35c6f62
Emmanuel Then <emmanuel.then@outlook.com>
2023-08-01 17:13:49 -0400

    added new dashboard sections

---
+++ app/dashboard/components/ChatSection.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ShinyText from '@/components/ShinyText'
+import PopoverButton from '@/components/PopoverButton'
+
+type Props = {}
+
+const ChatSection = (props: Props) => {
+    return (
+        <div>
+            <div className="p-5 lg:p-0 lg:flex lg:items-center lg:justify-between lg:ml-2 ">
+                <div className='text-center lg:text-left'>
+                    <ShinyText text="Live Chat" />
+                </div>
+                <div className='hidden lg:block notificationBell relative'>
+                    <div id='notificationDot' className='absolute h-[12px] w-[12px] bg-red9 rounded-full right-0 top-0' />
+                    <PopoverButton icon={
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor" className="w-5 h-5">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                        </svg>
+                    }
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ChatSection
--- app/dashboard/components/Dashboard.tsx
+++ app/dashboard/components/Dashboard.tsx
@@ -11,6 +11,10 @@ import AccountSection from './AccountSection'
 import MobileFullDashboard from './MobileFullDashboard'
 import InsightsSection from './InsightsSection'
 import DocumentsSection from './DocumentsSection'
+import UploadsSection from './UploadsSection'
+import FeedbackSection from './FeedbackSection'
+import ChatSection from './ChatSection'
+import SupportTicketsSections from './SupportTicketsSections'
 
 
 
@@ -36,7 +40,21 @@ const Dashboard = (props: Props) => {
         setValue('documents')
     }
 
-    
+    const handleUploadsClick = () => {
+        setValue('uploads')
+    }
+
+    const handleFeedbackClick = () => {
+        setValue('feedback')
+    }
+
+    const handleChatClick = () => {
+        setValue('chat')
+    }
+
+    const handleSupportTicketClick = () => {
+        setValue('supporttickets')
+    }
 
     return (
         <div className='flex w-full max-h-screen overflow-y-auto' id='full-dash'>
@@ -70,7 +88,7 @@ const Dashboard = (props: Props) => {
                             </div>
     </div>*/}
 
-                        <div id='loginTabs' className='hidden lg:flex flex-col justify-around h-full '>
+                        <div id='loginTabs' className='hidden lg:flex flex-col justify-around h-full mt-10 '>
 
                             <button
                                 value={value}
@@ -98,6 +116,37 @@ const Dashboard = (props: Props) => {
                             <button
                                 value={value}
                                 className='focus-selector p-1 flex items-center gap-2 hover:bg-slate2 hover:rounded-md '
+                                onClick={handleDocumentsClick}
+                            >
+                                <svg id='stroke-color' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" className="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                                </svg>
+                            </button>
+
+                            <button
+                                value={value}
+                                className='focus-selector p-1 flex items-center gap-2 hover:bg-slate2 hover:rounded-md '
+                                onClick={handleUploadsClick}
+                            >
+                                <svg id='stroke-color' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" className="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
+                                </svg>
+                            </button>
+
+                            <button
+                                value={value}
+                                className='focus-selector p-1 flex items-center gap-2 hover:bg-slate2 hover:rounded-md '
+                                onClick={handleFeedbackClick}
+                            >
+                                <svg id='stroke-color' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" className="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                                </svg>
+
+                            </button>
+
+                            <button
+                                value={value}
+                                className='focus-selector p-1 flex items-center gap-2 hover:bg-slate2 hover:rounded-md '
                                 onClick={handleInsightsClick}
                             >
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-6 h-6">
@@ -118,6 +167,49 @@ const Dashboard = (props: Props) => {
                             <button
                                 value={value}
                                 className='focus-selector p-1 flex items-center gap-2 hover:bg-slate2 hover:rounded-md '
+                                onClick={handleChatClick}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-6 h-6">
+                                    <defs>
+                                        <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                                            <stop offset="0%" stopColor="hsl(358, 75%, 59%)" />
+                                            <stop offset="25%" stopColor="purple" />
+                                            <stop offset="50%" stopColor="pink" />
+                                            <stop offset="75%" stopColor="rgb(64, 64, 234)" />
+                                            <stop offset="100%" stopColor="#3b82f6" />
+                                        </linearGradient>
+                                    </defs>
+                                    <path strokeLinecap="round" strokeLinejoin="round" stroke="url(#gradient)" strokeWidth="1.2" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
+                                </svg>
+
+                            </button>
+
+                            <button
+                                value={value}
+                                className='focus-selector p-1 flex items-center gap-2 hover:bg-slate2 hover:rounded-md '
+                                onClick={handleSupportTicketClick}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-6 h-6">
+                                    <defs>
+                                        <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                                            <stop offset="0%" stopColor="hsl(358, 75%, 59%)" />
+                                            <stop offset="25%" stopColor="purple" />
+                                            <stop offset="50%" stopColor="pink" />
+                                            <stop offset="75%" stopColor="rgb(64, 64, 234)" />
+                                            <stop offset="100%" stopColor="#3b82f6" />
+                                        </linearGradient>
+                                    </defs>
+                                    <path strokeLinecap="round" strokeLinejoin="round" stroke="url(#gradient)" strokeWidth="1.2" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z" />
+                                </svg>
+
+                            </button>
+
+                            
+                            
+
+                            <button
+                                value={value}
+                                className='focus-selector p-1 flex items-center gap-2 hover:bg-slate2 hover:rounded-md '
                                 onClick={handleAccountClick}
                             >
                                 <svg id='stroke-color' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" className="w-6 h-6">
@@ -127,19 +219,9 @@ const Dashboard = (props: Props) => {
 
                             </button>
 
-                            <button
-                                value={value}
-                                className='focus-selector p-1 flex items-center gap-2 hover:bg-slate2 hover:rounded-md '
-                                onClick={handleDocumentsClick}
-                            >
-                                <svg id='stroke-color' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" className="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                                </svg>
-                            </button>
-                            
 
                         </div>
-                        <div className='hidden lg:block'>
+                        {/* <div className='hidden lg:block'>
                             <form action="/auth/signout" method="post">
                                 <button type='submit' className='mb-5 hover:cursor-pointer hover:bg-slate2 hover:rounded-md  inline-flex h-[35px] items-center justify-center gap-2 rounded-[4px] p-1 font-light leading-none focus:outline-none' >
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="hsl(358 75% 59%)" className="w-6 h-6">
@@ -147,7 +229,7 @@ const Dashboard = (props: Props) => {
                                     </svg>
                                 </button>
                             </form>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
                 <div className='lg:hidden w-[100vw] h-screen'>
@@ -167,6 +249,14 @@ const Dashboard = (props: Props) => {
                             return <AccountSection />;
                         case 'documents':
                             return <DocumentsSection />
+                        case 'uploads':
+                            return <UploadsSection />
+                        case 'feedback':
+                            return <FeedbackSection />
+                        case 'chat':
+                            return <ChatSection />
+                        case 'supporttickets':
+                            return <SupportTicketsSections />
                         default:
                             return null; // Render nothing or fallback UI
                     }
