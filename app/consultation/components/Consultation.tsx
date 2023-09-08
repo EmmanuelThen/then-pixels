05cf75461ffc5a94eb373966d34748aa6fc0be83
Emmanuel Then <emmanuel.then@outlook.com>
2023-09-07 21:23:46 -0400

    updates to consultation page

--- app/consultation/components/Consultation.tsx
+++ app/consultation/components/Consultation.tsx
@@ -13,19 +13,19 @@ type Props = {}
 
 const Consultation = (props: Props) => {
     return (
-        <div>
-            <div className='flex flex-col items-center justify-center p-5 md:p-20'>
+        <div className='md:fixed md:overflow-y-hidden'>
+            <div className='flex flex-col items-center justify-center p-5 md:p-10'>
                 <div>
                     <ShinyText
                         text='Consultation'
                     />
                 </div>
-                <div className='flex flex-col md:flex-row rounded shadow-blackA9 shadow-[0px_4px_7px] w-full'>
-                    <div className='color-change-5x flex justify-center items-center rounded-t p-10 md:rounded-l md:w-[60%]'>
+                <div className='flex flex-col md:flex-row rounded shadow-blackA9 shadow-[0px_4px_7px] w-full md:h-[60vh] lg:h-[80vh]'>
+                    <div className='color-change-5x flex justify-center items-center rounded-t md:rounded-tr-none p-10 md:rounded-l md:w-[60%]'>
                         ThenPixels logo here
                     </div>
                     {/* make this section hidden behind the left side and have it slide in on page load */}
-                    <div className='flex justify-center items-center md:w-[40%] p-5 '>
+                    <div className='flex justify-center items-center md:w-[40%] p-5 md:overflow-y-scroll '>
                         <ConsultationForm
                             valueMissingMessage={undefined}
                             typeMismatchMessage={undefined}
--- app/consultation/components/ConsultationForm.tsx
+++ app/consultation/components/ConsultationForm.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import * as Form from '@radix-ui/react-form';
 import * as RadioGroup from '@radix-ui/react-radio-group';
+import TextArea from '@/components/ui/TextArea/TextArea';
 
 
 type Props = {
@@ -11,11 +12,32 @@ type Props = {
 
 const ConsultationForm = ({ valueMissingMessage, typeMismatchMessage, buttonText, }: Props) => {
     return (
-        <Form.Root className="w-full p-2">
+        <Form.Root className="w-full p-2 md:h-full">
             {/* Field 1 */}
+            <h1 className='text-blue9 font-semibold mb-5'>
+                Contact Information:
+            </h1>
             <Form.Field className="grid" name="full_name">
                 <div className="flex items-baseline justify-between">
                     <Form.Label className="text-[15px] font-medium leading-[35px] text-slate10">Full name</Form.Label>
+                    <Form.Message className="text-[13px] text-red9" match="valueMissing">
+                        {valueMissingMessage}
+                    </Form.Message>
+                    <Form.Message className="text-[13px] text-red9" match="typeMismatch">
+                        {typeMismatchMessage}
+                    </Form.Message>
+                </div>
+                <Form.Control asChild>
+                    <input
+                        className="mb-5 box-border w-full bg-slate4 shadow-blackA9 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA9"
+                        type='text'
+                        required
+                    />
+                </Form.Control>
+            </Form.Field>
+            <Form.Field name='business_industry'>
+                <div className="flex items-baseline justify-between">
+                    <Form.Label className="text-[15px] font-medium leading-[35px] text-slate10">Company name</Form.Label>
                     <Form.Message className="text-[13px] text-red9" match="valueMissing">
                         {valueMissingMessage}
                     </Form.Message>
@@ -69,12 +91,18 @@ const ConsultationForm = ({ valueMissingMessage, typeMismatchMessage, buttonText
                     />
                 </Form.Control>
             </Form.Field>
+            <h1 className='text-blue9 font-semibold mb-5'>
+                Project overview:
+            </h1>
             <Form.Field name='website_format'>
                 <RadioGroup.Root
                     className="flex flex-col gap-2.5 mb-5"
                     defaultValue="5"
                     aria-label="View density"
                 >
+                    <h1 className='text-slate10 font-medium'>
+                        What type of website are you looking to create?
+                    </h1>
                     <div className="flex items-center">
                         <RadioGroup.Item
                             className="hover:cursor-pointer bg-white w-[25px] h-[25px] rounded-full shadow-[0_2px_10px] shadow-blackA7  focus:shadow-[0_0_0_2px] focus:shadow-black outline-none cursor-default"
@@ -125,11 +153,190 @@ const ConsultationForm = ({ valueMissingMessage, typeMismatchMessage, buttonText
                     </div>
                 </RadioGroup.Root>
             </Form.Field>
+            <Form.Field name='text_area'>
+                <div className="flex items-baseline justify-between">
+                    <Form.Label className="text-[15px] font-medium leading-[35px] text-slate10">
+                        Who is your target audience or customer base?
+                    </Form.Label>
+                    <Form.Message className="text-[13px] text-red9" match="valueMissing">
+                        {valueMissingMessage}
+                    </Form.Message>
+                    <Form.Message className="text-[13px] text-red9" match="typeMismatch">
+                        {typeMismatchMessage}
+                    </Form.Message>
+                </div>
+                <Form.Control asChild>
+                    <input
+                        className="mb-5 box-border w-full bg-slate4 shadow-blackA9 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA9"
+                        type='text'
+                        required
+                    />
+                </Form.Control>
+            </Form.Field>
+            <Form.Field name='text_area'>
+                <div className="flex items-baseline justify-between">
+                    <Form.Label className="text-[15px] font-medium leading-[25px] mb-2 text-slate10">
+                        Are there any websites or designs that you admire and would like to use as inspiration?
+                        If so list the website
+                    </Form.Label>
+                    <Form.Message className="text-[13px] text-red9" match="valueMissing">
+                        {valueMissingMessage}
+                    </Form.Message>
+                    <Form.Message className="text-[13px] text-red9" match="typeMismatch">
+                        {typeMismatchMessage}
+                    </Form.Message>
+                </div>
+                <Form.Control asChild>
+                    <input
+                        className="mb-5 box-border w-full bg-slate4 shadow-blackA9 inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA9"
+                        type='text'
+                        required
+                    />
+                </Form.Control>
+            </Form.Field>
+            <Form.Field name='text_area'>
+                <div className="flex items-baseline justify-between">
+                    <Form.Label className="text-[15px] font-medium leading-[25px] mb-2 text-slate10">
+                        Do you have a brand style guide or existing branding elements (logo, color scheme)
+                        that should be incorporated into the website?
+                    </Form.Label>
+                    <Form.Message className="text-[13px] text-red9" match="valueMissing">
+                        {valueMissingMessage}
+                    </Form.Message>
+                    <Form.Message className="text-[13px] text-red9" match="typeMismatch">
+                        {typeMismatchMessage}
+                    </Form.Message>
+                </div>
+                <RadioGroup.Root
+                    className='flex justify-around mb-5'
+                    defaultValue='yes'
+                >
+                    <div className="flex items-center">
+                        <RadioGroup.Item
+                            className="hover:cursor-pointer bg-white w-[25px] h-[25px] rounded-full shadow-[0_2px_10px] shadow-blackA7  focus:shadow-[0_0_0_2px] focus:shadow-black outline-none cursor-default"
+                            value="1"
+                            id="yes"
+                        >
+                            <RadioGroup.Indicator className="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-[11px] after:h-[11px] after:rounded-[50%] after:bg-blue9" />
+                        </RadioGroup.Item>
+                        <label className=" text-[15px] leading-none pl-[15px]" >
+                            Yes
+                        </label>
+                    </div>
+                    <div className="flex items-center">
+                        <RadioGroup.Item
+                            className="hover:cursor-pointer bg-white w-[25px] h-[25px] rounded-full shadow-[0_2px_10px] shadow-blackA7  focus:shadow-[0_0_0_2px] focus:shadow-black outline-none cursor-default"
+                            value="2"
+                            id="no"
+                        >
+                            <RadioGroup.Indicator className="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-[11px] after:h-[11px] after:rounded-[50%] after:bg-blue9" />
+                        </RadioGroup.Item>
+                        <label className=" text-[15px] leading-none pl-[15px]" >
+                            No
+                        </label>
+                    </div>
+                </RadioGroup.Root>
+            </Form.Field>
+            <Form.Field name='text_area'>
+                <div className="flex items-baseline justify-between">
+                    <Form.Label className="text-[15px] font-medium leading-[25px] mb-2 text-slate10">
+                        Do you have existing content (text, images, videos) that you want to include on the website?
+                    </Form.Label>
+                    <Form.Message className="text-[13px] text-red9" match="valueMissing">
+                        {valueMissingMessage}
+                    </Form.Message>
+                    <Form.Message className="text-[13px] text-red9" match="typeMismatch">
+                        {typeMismatchMessage}
+                    </Form.Message>
+                </div>
+                <RadioGroup.Root
+                    className='flex justify-around mb-5'
+                    defaultValue='yes'
+                >
+                    <div className="flex items-center">
+                        <RadioGroup.Item
+                            className="hover:cursor-pointer bg-white w-[25px] h-[25px] rounded-full shadow-[0_2px_10px] shadow-blackA7  focus:shadow-[0_0_0_2px] focus:shadow-black outline-none cursor-default"
+                            value="1"
+                            id="yes"
+                        >
+                            <RadioGroup.Indicator className="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-[11px] after:h-[11px] after:rounded-[50%] after:bg-blue9" />
+                        </RadioGroup.Item>
+                        <label className=" text-[15px] leading-none pl-[15px]" >
+                            Yes
+                        </label>
+                    </div>
+                    <div className="flex items-center">
+                        <RadioGroup.Item
+                            className="hover:cursor-pointer bg-white w-[25px] h-[25px] rounded-full shadow-[0_2px_10px] shadow-blackA7  focus:shadow-[0_0_0_2px] focus:shadow-black outline-none cursor-default"
+                            value="2"
+                            id="no"
+                        >
+                            <RadioGroup.Indicator className="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-[11px] after:h-[11px] after:rounded-[50%] after:bg-blue9" />
+                        </RadioGroup.Item>
+                        <label className=" text-[15px] leading-none pl-[15px]" >
+                            No
+                        </label>
+                    </div>
+                </RadioGroup.Root>
+            </Form.Field>
+            <Form.Field name='text_area' className='mb-5'>
+                <div className="flex items-baseline justify-between">
+                    <Form.Label className="text-[15px] font-medium leading-[25px] mb-2 text-slate10">
+                        Briefly describe your project & what specific goals or objectives you are looking to achieve with this project?
+                    </Form.Label>
+                    <Form.Message className="text-[13px] text-red9" match="valueMissing">
+                        {valueMissingMessage}
+                    </Form.Message>
+                    <Form.Message className="text-[13px] text-red9" match="typeMismatch">
+                        {typeMismatchMessage}
+                    </Form.Message>
+                </div>
+                <Form.Control asChild>
+                    <TextArea />
+                </Form.Control>
+            </Form.Field>
+
+            <Form.Field className="grid" name="full_name">
+                <div className="flex items-baseline justify-between">
+                    <Form.Label className="text-[15px] font-medium leading-[25px] mb-2 text-slate10">
+                        What specific features or third-party integrations do you want on your website (e.g., contact forms,
+                        e-commerce functionality, blog, user registration, payment gateways, CRM)?
+                    </Form.Label>
+                    <Form.Message className="text-[13px] text-red9" match="valueMissing">
+                        {valueMissingMessage}
+                    </Form.Message>
+                    <Form.Message className="text-[13px] text-red9" match="typeMismatch">
+                        {typeMismatchMessage}
+                    </Form.Message>
+                </div>
+                <Form.Control asChild>
+                    <TextArea />
+                </Form.Control>
+            </Form.Field>
+            <h1 className='text-blue9 font-semibold mt-5 mb-5'>
+                Additional Comments or Questions:
+            </h1>
+            <Form.Field className="grid" name="full_name">
+                <div className="flex items-baseline justify-between">
+                    <Form.Label className="text-[15px] font-medium leading-[25px] mb-2 text-slate10">
+                        Is there any other information, questions, or special requests you would like to share?
+                    </Form.Label>
+                    <Form.Message className="text-[13px] text-red9" match="valueMissing">
+                        {valueMissingMessage}
+                    </Form.Message>
+                    <Form.Message className="text-[13px] text-red9" match="typeMismatch">
+                        {typeMismatchMessage}
+                    </Form.Message>
+                </div>
+                <Form.Control asChild>
+                    <TextArea />
+                </Form.Control>
+            </Form.Field>
             <Form.Submit asChild>
                 <button
                     type='submit'
                     disabled={true}
-                    className={`w-full box-border text-white hover:opacity-80 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-blue9 px-[15px] font-medium leading-none mt-[10px] cursor-pointer`}
+                    className={`mb-5 w-full box-border text-white hover:opacity-80 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-blue9 px-[15px] font-medium leading-none mt-[10px] cursor-pointer`}
                 >
                     {buttonText}
                 </button>
